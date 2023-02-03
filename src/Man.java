b339c9774c1eb5f58ac56a9ac6ea8d56685b294a
Artyom Sapegin <sapeginartyom@gmail.com>
2023-02-03 16:59:07 +0300

    Fix bug in Man.java

--- src/Man.java
+++ src/Man.java
@@ -1,6 +1,22 @@
 public class Man {
-    int age;
-    String name;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int age;
+    private String name;
 
     public Man(int age, String name) {
         this.age = age;
